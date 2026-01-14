b99fdb6582b7e9a1ee2474da62b873692a4b2ea8
Leo Claudio <johnleoclaudio@users.noreply.github.com>
2026-01-14 14:32:45 +0800

    chore: update configs

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,3 @@
--- return {
---   "telescope.nvim",
---   dependencies = {
---     "nvim-telescope/telescope-fzf-native.nvim",
---     build = "make",
---     config = function()
---       require("telescope").load_extension("fzf")
---     end,
---   },
--- }
---
-
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
